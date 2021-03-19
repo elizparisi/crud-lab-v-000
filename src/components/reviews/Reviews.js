382d20eb11bdb7e728aa8b640db9f990bf0aa4a8
Liz P <parisielizabeth@gmail.com>
2021-03-19 03:44:46 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,37 +1,16 @@
-// import React, { Component } from 'react';
-// import Review from './Review';
-
-// class Reviews extends Component {
-//   render() {
-//     return (
-//       <ul>
-//         {this.props.reviews.map((review) => (
-//           <Review review={review} key={review.id} deleteReview={this.props.deleteReview} />
-//         ))}
-//       </ul>
-//     );
-//   }
-// };
-
-// export default Reviews;
-
-import React, { Component } from "react";
-import Review from "./Review";
+import React, { Component } from 'react';
+import Review from './Review';
 
 class Reviews extends Component {
   render() {
     return (
       <ul>
         {this.props.reviews.map((review) => (
-          <Review
-            review={review}
-            key={review.id}
-            deleteReview={this.props.deleteReview}
-          />
+          <Review review={review} key={review.id} deleteReview={this.props.deleteReview} />
         ))}
       </ul>
     );
   }
-}
+};
 
 export default Reviews;
