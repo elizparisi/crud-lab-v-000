c04f108126f18672c4475d9aab4a0e3f5484d551
Liz P <parisielizabeth@gmail.com>
2021-03-19 03:54:23 +0000

    Automatically backed up by Learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
-import Reviews from './Reviews';
+// import React, { Component } from 'react';
+// import Reviews from './Reviews';
 
-class ReviewInput extends Component {
+// class ReviewInput extends Component {
   
-  state = {
-    text: ''
-  };
+//   state = {
+//     text: ''
+//   };
   
-  handleOnChange = event => {
-    this.setState({
-      text: event.target.value
-    });
-  }
+//   handleOnChange = event => {
+//     this.setState({
+//       text: event.target.value
+//     });
+//   }
   
-  handleOnSubmit = event => {
-    event.preventDefault();
-    const review = {
-      text: this.state.text,
-      restaurantId: this.props.restaurantId,
-    };
-    this.props.addReview(this.state.text);
-    this.setState({
-      text: '',
-    })
-  }
+//   handleOnSubmit = event => {
+//     event.preventDefault();
+//     const review = {
+//       text: this.state.text,
+//       restaurantId: this.props.restaurantId,
+//     };
+//     this.props.addReview(this.state.text);
+//     this.setState({
+//       text: '',
+//     })
+//   }
   
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleOnSubmit}>
-          <input type="text" value={this.state.text}
-            onChange={this.handleOnChange} />
-          <input type="submit" />
-        </form>
-      </div>
-    );
-  }
-};
+//   render() {
+//     return (
+//       <div>
+//         <form onSubmit={this.handleOnSubmit}>
+//           <input type="text" value={this.state.text}
+//             onChange={this.handleOnChange} />
+//           <input type="submit" />
+//         </form>
+//       </div>
+//     );
+//   }
+// };
 
-export default ReviewInput;
+// export default ReviewInput;
 
 
 //   handleOnSubmit = event => {
