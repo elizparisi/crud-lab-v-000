8be890c18acf9e3aae2fbb5f061821ccb396a208
Liz P <parisielizabeth@gmail.com>
2021-03-19 03:44:30 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -14,3 +14,24 @@
 // };
 
 // export default Reviews;
+
+import React, { Component } from "react";
+import Review from "./Review";
+
+class Reviews extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.reviews.map((review) => (
+          <Review
+            review={review}
+            key={review.id}
+            deleteReview={this.props.deleteReview}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Reviews;
