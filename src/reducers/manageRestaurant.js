0b4521495e0c41edc13cafdbcbc3b39d83640815
Liz P <parisielizabeth@gmail.com>
2021-03-19 03:36:47 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,51 +1,51 @@
-import cuid from 'cuid';
+// import cuid from 'cuid';
 
-export default function manageRestaurants(
-  state = {
-    restaurants: [],
-    reviews: [],
-  },action) {
-    switch (action.type) {
+// export default function manageRestaurants(
+//   state = {
+//     restaurants: [],
+//     reviews: [],
+//   },action) {
+//     switch (action.type) {
       
-    case "ADD_RESTAURANT":
-      const restaurant = {
-        text: action.restaurant,
-        id: cuid(),
-      };
+//     case "ADD_RESTAURANT":
+//       const restaurant = {
+//         text: action.restaurant,
+//         id: cuid(),
+//       };
 
-      return {
-        ...state,
-        restaurants: [...state.restaurants, restaurant],
-      };
+//       return {
+//         ...state,
+//         restaurants: [...state.restaurants, restaurant],
+//       };
     
-    case "DELETE_RESTAURANT":
-      const restaurants = state.restaurants.filter(
-        (restaurant) => restaurant.id !== action.id
-      );
-      return {
-        ...state,
-        restaurants,
-      };
+//     case "DELETE_RESTAURANT":
+//       const restaurants = state.restaurants.filter(
+//         (restaurant) => restaurant.id !== action.id
+//       );
+//       return {
+//         ...state,
+//         restaurants,
+//       };
     
-    case "ADD_REVIEW":
-      const review = {
-        id: cuid(),
-        restaurantId: action.review.restaurantId,
-        text: action.review.text,
-      };
-      return {
-        ...state,
-        reviews: [...state.reviews, review],
-      };
+//     case "ADD_REVIEW":
+//       const review = {
+//         id: cuid(),
+//         restaurantId: action.review.restaurantId,
+//         text: action.review.text,
+//       };
+//       return {
+//         ...state,
+//         reviews: [...state.reviews, review],
+//       };
     
-    case "DELETE_REVIEW":
-      const reviews = state.reviews.filter((review) => review.id !== action.id);
-      return {
-        ...state,
-        reviews,
-      };
+//     case "DELETE_REVIEW":
+//       const reviews = state.reviews.filter((review) => review.id !== action.id);
+//       return {
+//         ...state,
+//         reviews,
+//       };
     
-    default:
-      return state;
-  }
-}
+//     default:
+//       return state;
+//   }
+// }
