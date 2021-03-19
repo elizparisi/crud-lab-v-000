edb7839d0b54305ac0d2ee4f23c4d5660598e74c
Liz P <parisielizabeth@gmail.com>
2021-03-19 03:37:21 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,87 +1,51 @@
-// import cuid from 'cuid';
-
-// export default function manageRestaurants(
-//   state = {
-//     restaurants: [],
-//     reviews: [],
-//   },action) {
-//     switch (action.type) {
-      
-//     case "ADD_RESTAURANT":
-//       const restaurant = {
-//         text: action.restaurant,
-//         id: cuid(),
-//       };
-
-//       return {
-//         ...state,
-//         restaurants: [...state.restaurants, restaurant],
-//       };
-    
-//     case "DELETE_RESTAURANT":
-//       const restaurants = state.restaurants.filter(
-//         (restaurant) => restaurant.id !== action.id
-//       );
-//       return {
-//         ...state,
-//         restaurants,
-//       };
-    
-//     case "ADD_REVIEW":
-//       const review = {
-//         id: cuid(),
-//         restaurantId: action.review.restaurantId,
-//         text: action.review.text,
-//       };
-//       return {
-//         ...state,
-//         reviews: [...state.reviews, review],
-//       };
-    
-//     case "DELETE_REVIEW":
-//       const reviews = state.reviews.filter((review) => review.id !== action.id);
-//       return {
-//         ...state,
-//         reviews,
-//       };
-    
-//     default:
-//       return state;
-//   }
-// }
-
 import cuid from 'cuid';
-export const cuidFn = cuid;
 
-export default function manageRestaurants(state ={
+export default function manageRestaurants(
+  state = {
     restaurants: [],
-    reviews: []
-}, action) {
+    reviews: [],
+  },action) {
     switch (action.type) {
+      
+    case "ADD_RESTAURANT":
+      const restaurant = {
+        text: action.restaurant,
+        id: cuid(),
+      };
 
-        case 'ADD_RESTAURANT':
-            const restaurant = {text: action.text, id: cuidFn()}
-                return {
-                ...state, 
-                restaurants: [...state.restaurants, restaurant]
-                }
-
-        case 'DELETE_RESTAURANT':
-            const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
-            return {...state, restaurants}
-
-
-        case 'ADD_REVIEW':
-            const review ={text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn()};
-                return {...state,
-                    reviews: [...state.reviews, review]
-                }
-
-        case 'DELETE_REVIEW':
-            const reviews = state.reviews.filter(review => review.id !== action.id);
-                return {...state, reviews}
-
-        default: 
-        return state;
-    }
+      return {
+        ...state,
+        restaurants: [...state.restaurants, restaurant],
+      };
+    
+    case "DELETE_RESTAURANT":
+      const restaurants = state.restaurants.filter(
+        (restaurant) => restaurant.id !== action.id
+      );
+      return {
+        ...state,
+        restaurants,
+      };
+    
+    case "ADD_REVIEW":
+      const review = {
+        id: cuid(),
+        restaurantId: action.review.restaurantId,
+        text: action.review.text,
+      };
+      return {
+        ...state,
+        reviews: [...state.reviews, review],
+      };
+    
+    case "DELETE_REVIEW":
+      const reviews = state.reviews.filter((review) => review.id !== action.id);
+      return {
+        ...state,
+        reviews,
+      };
+    
+    default:
+      return state;
+  }
 }
